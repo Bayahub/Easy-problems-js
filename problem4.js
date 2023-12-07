3052f5ce07d69d197eab3e3a6a88fbeb5cdc5210
Behailu Demise <hiriyan2009@gmail.com>
2023-12-07 17:25:45 -0500

    solution to problem5

---
+++ problem4.js
@@ -0,0 +1,3 @@
+const text="abcd"
+const reversedText 
+
---
+++ problem5.js
@@ -0,0 +1,3 @@
+const names=["Abebe","Bekele","Chala","Debeb","Ermias","belete","behailu"];
+const numberofnames=names.length;
+console.log(numberofnames);
