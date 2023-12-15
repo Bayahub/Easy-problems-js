4c8e9ca896196c6b53da630f84572fcf62b40756
Behailu Demise <hiriyan2009@gmail.com>
2023-12-15 09:24:07 -0500

    solution to problem24

---
+++ problem23.js
@@ -0,0 +1,9 @@
+
+function isLess(a + b<30){
+{
+        return true;}
+        else{ return false}
+    }
+    console.log(a,b)
+
+console.log(x, y)
---
+++ problem24.js
@@ -0,0 +1,9 @@
+let num =(n1, n2)=>{
+    if(n1===n2) {
+        return true;
+        }
+        else{
+            return false;
+        }
+}
+console.log(num(15,10))
