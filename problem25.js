53350f548c8ee2dce0021db1463a575763bd9788
Behailu Demise <hiriyan2009@gmail.com>
2023-12-15 09:42:13 -0500

    solution to problem25

---
+++ problem25.js
@@ -0,0 +1,8 @@
+function calculator(exp) {
+    return eval(exp);
+    }
+    
+    console.log(calculator("23+4"));
+    console.log(calculator("45-15"));
+    console.log(calculator("13+2-5*2"));
+    console.log(calculator("49/7*2-3"));
