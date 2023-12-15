1ee32774b8965acb61e996c1388414b9ded166df
Behailu Demise <hiriyan2009@gmail.com>
2023-12-15 10:16:20 -0500

    solution to problem26

---
+++ problem26.js
@@ -0,0 +1,9 @@
+let string = (stg1, stg2) =>{
+    if (stg1.length===stg2.length) {
+        return true; 
+    }
+            else {
+                return false;
+            }
+        }   
+console.log(string("home", "made"))
